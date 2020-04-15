a863e5e51e8e3e56e3fb537ff1fcd85c01d37b76
rkr33 <52146681+rkr33@users.noreply.github.com>
2020-04-15 10:27:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post('/', (req, res) => {
 });
 
 // Define port
-const port = 3000;
+const port = process.env.PORT|| 3000;
 
 // Start server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
